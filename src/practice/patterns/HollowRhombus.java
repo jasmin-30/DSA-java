640f666f81485b0618a3c7e2a23a352c4e7d8a1b
Jasmin Makwana <jasminmakwana1999@gmail.com>
2022-07-04 22:34:03 +0530

    Day 4 : Advanced Patterns

---
+++ src/practice/patterns/HollowRhombus.java
@@ -0,0 +1,37 @@
+package practice.patterns;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+for n = 5
+
+    *****
+   *   *
+  *   *
+ *   *
+*****
+
+
+ */
+public class HollowRhombus {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+
+        for (int i=1;i<=n;i++) {
+            for (int j=1;j<=(n-i);j++) {
+                System.out.print(" ");
+            }
+
+            for (int j=1;j<=n;j++) {
+                if (i==1 || i == n || j == 1 || j == n)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/practice/patterns/SolidRhombus.java
@@ -0,0 +1,33 @@
+package practice.patterns;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+for n = 5
+
+    *****
+   *****
+  *****
+ *****
+*****
+
+ */
+public class SolidRhombus {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+
+        for (int i=1;i<=n;i++) {
+            for (int j=1;j<=(n-i);j++) {
+                System.out.print(" ");
+            }
+
+            for (int j=1;j<=n;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
