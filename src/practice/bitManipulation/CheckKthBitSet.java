4e342a57467c185fb0675bd78f5dd72a368843d7
Jasmin Makwana <jasminmakwana1999@gmail.com>
2022-07-11 09:52:18 +0530

    Day 7 : Bit Manipulation: Check Kth bit set

--- src/practice/bitManipulation/CheckKthBitSet.java
+++ src/practice/bitManipulation/CheckKthBitSet.java
@@ -2,13 +2,17 @@ package practice.bitManipulation;
 
 public class CheckKthBitSet {
     public static void main(String[] args) {
-        System.out.println(checkKthBit(5, 3));
-        System.out.println(checkKthBit(5, 2));
-        System.out.println(checkKthBit(10, 2));
-        System.out.println(checkKthBit(10, 3));
+        System.out.println(checkKthBitUsingLeftShift(5, 3));
+        System.out.println(checkKthBitUsingLeftShift(5, 2));
+        System.out.println(checkKthBitUsingRightShift(10, 2));
+        System.out.println(checkKthBitUsingRightShift(10, 3));
     }
 
-    public static boolean checkKthBit(int n, int k) {
+    public static boolean checkKthBitUsingLeftShift(int n, int k) {
         return (n & (1 << (k-1))) != 0;
+    }
+
+    public static boolean checkKthBitUsingRightShift(int n, int k) {
+        return ((n >> (k-1)) & 1) == 1;
     }
 }
