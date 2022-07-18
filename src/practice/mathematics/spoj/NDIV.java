dbd5f04a348682d83862e13b0ba5845fd4caac2c
Jasmin Makwana <jasminmakwana1999@gmail.com>
2022-07-18 21:33:04 +0530

    SPOJ: NDIV - Segmented Sieve approach

--- src/practice/mathematics/spoj/NDIV.java
+++ src/practice/mathematics/spoj/NDIV.java
@@ -5,7 +5,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class NDIV {
+    private static int maxN = 100000;
+    private static boolean[] primes = new boolean[maxN + 1];
+    private static int[] primeSieve = new int[maxN+1];
+
+    private static void createSieve() {
+        int k=0;
+        for (int i=2;i*i<=maxN;i++) {
+            if (!primes[i]) {
+                primeSieve[k++] = i;
+                for (int j=2*i;j<=maxN;j+=i) {
+                    primes[j] = true;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
+        createSieve();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String[] str_a = reader.readLine().split(" ");
         long a = Long.parseLong(str_a[0]);
@@ -19,15 +36,21 @@ public class NDIV {
         int res = 0;
 
         for (long i = a; i<=b; i++) {
-            int count = 0;
-            for (long j=1;j*j<=i;j++) {
-                if (i % j == 0) {
-                    if (i / j == j)
-                        count += 1;
-                    else
-                        count += 2;
+            int count = 1;
+            long num = i;
+            long primeFactor = primeSieve[0];
+            int k = 1;
+            while (primeFactor * primeFactor <=num) {
+                int cnt = 0;
+                while (num % primeFactor == 0) {
+                    cnt++;
+                    num = num / primeFactor;
                 }
+                count = count * (cnt + 1);
+                primeFactor = primeSieve[k++];
             }
+            if (num != 1) count *= 2;
+
             if (count == n)
                 res+=1;
         }
