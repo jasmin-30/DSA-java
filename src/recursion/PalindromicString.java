edf8a9c714901b62b799538059863a5f902155d9
Jasmin Makwana <jasminmakwana1999@gmail.com>
2022-07-19 09:47:48 +0530

    Recursion: Palindromic String

---
+++ src/recursion/PalindromicString.java
@@ -0,0 +1,23 @@
+package recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+For a given string check whether string is palindrome or not.
+ */
+public class PalindromicString {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        System.out.println(isPalindrome(s, 0, s.length()-1));
+    }
+
+    private static boolean isPalindrome(String s, int start, int end) {
+        if (start >= end)
+            return s.charAt(start) == s.charAt(end);
+        else
+            return isPalindrome(s, start+1, end-1);
+    }
+}
