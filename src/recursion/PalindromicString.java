880defb434f1c4deeeb9a465a17166241c7fa88e
Jasmin Makwana <jasminmakwana1999@gmail.com>
2022-07-19 09:49:59 +0530

    Recursion: Palindromic String - improved

--- src/recursion/PalindromicString.java
+++ src/recursion/PalindromicString.java
@@ -16,8 +16,11 @@ public class PalindromicString {
 
     private static boolean isPalindrome(String s, int start, int end) {
         if (start >= end)
-            return s.charAt(start) == s.charAt(end);
-        else
-            return isPalindrome(s, start+1, end-1);
+            return true;
+
+        if (s.charAt(start) != s.charAt(end))
+            return false;
+
+        return isPalindrome(s, start + 1, end - 1);
     }
 }
