c72bb64cd1f39d5973fc2690ba8d0e7a2434522a
Jasmin Makwana <jasminmakwana1999@gmail.com>
2022-07-23 11:55:57 +0530

    Leetcode: power-of-two

--- src/practice/leetcode/recursion/PowerOfTwo.java
+++ src/practice/leetcode/recursion/PowerOfTwo.java
@@ -12,14 +12,12 @@ public class PowerOfTwo {
     }
 
     public static boolean isPowerOfTwo(int n) {
-        if (n == 1)
+        if (n==2 || n == 1)
             return true;
-        if (n == 0)
+        if (n <= 0 || n%2 != 0)
             return false;
 
-        if (n%2 == 0)
-            return isPowerOfTwo(n/2);
+        return isPowerOfTwo(n/2);
 
-        return false;
     }
 }
