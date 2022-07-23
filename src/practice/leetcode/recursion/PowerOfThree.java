e80fce02335923cef029aa7752b773f387e20f8d
Jasmin Makwana <jasminmakwana1999@gmail.com>
2022-07-23 11:55:17 +0530

    Leetcode: power-of-three

--- src/practice/leetcode/recursion/PowerOfThree.java
+++ src/practice/leetcode/recursion/PowerOfThree.java
@@ -12,14 +12,11 @@ public class PowerOfThree {
     }
 
     public static boolean isPowerOfThree(int n) {
-        if (n == 1)
+        if (n==3 || n == 1)
             return true;
-        if (n == 0)
+        if (n <= 0 || n%3 != 0)
             return false;
 
-        if (n%3 == 0)
-            return isPowerOfThree(n/3);
-
-        return false;
+        return isPowerOfThree(n/3);
     }
 }
