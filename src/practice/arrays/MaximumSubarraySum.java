c0bcb9959aa7356af9fd42c2a120ecc217ba2331
Jasmin Makwana <jasminmakwana1999@gmail.com>
2022-07-27 09:51:34 +0530

    Arrays: Maximum Subarray sum : Naive solution

---
+++ src/practice/arrays/MaximumSubarraySum.java
@@ -0,0 +1,48 @@
+package practice.arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+Given an array, we need to find maximum sum of a subarray.
+
+Example:
+
+I/P: [2, 3, -8, 7, -1, 2, 3]
+O/P: 11
+exp: sum of [7, -1, 2, 3] = 11 which is maximum sum of any subarray
+
+I/P: [5, 8, 3]
+O/P: 16
+exp: sum of [5, 8, 3] = 16 which is maximum sum of any subarray
+
+I/P: [-6, -1, -8]
+O/P: -1
+exp: sum of [-1] = -1 which is maximum sum of any subarray
+ */
+public class MaximumSubarraySum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        String[] str_a = reader.readLine().split(" ");
+        int[] arr = new int[n];
+        for (int i=0;i<n;i++) {
+            arr[i] = Integer.parseInt(str_a[i]);
+        }
+        System.out.println(maxSubarraySum1(arr, n));
+    }
+
+    private static int maxSubarraySum1(int[] arr, int n) {
+        int res = Integer.MIN_VALUE;
+
+        for (int i=0;i<n;i++) {
+            int currSum = 0;
+            for (int j=i;j<n;j++) {
+                currSum += arr[j];
+                res = Math.max(res, currSum);
+            }
+        }
+        return res;
+    }
+}
